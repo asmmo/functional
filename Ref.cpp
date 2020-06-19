47e3eb5e28020617b2217ab0c30aa0aef0ad9a5c
Ammar Sayed Mohammed Mohammed <ammarsayed99ph@gmail.com>
2020-06-20 00:55:54 +0200

    Update Ref.cpp

--- Ref.cpp
+++ Ref.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
+
 template <class T>
 struct refWrapper{
-    operator T&(){
+    operator T&() {
         return member;
     }
-    explicit myRef(T& ref):member{ref}{}
+    explicit refWrapper(T& ref):member{ref}{}
 private:
     T& member;
 };
@@ -11,4 +13,22 @@ template <class T>
 refWrapper<T> myRef( T& arg)
 {
     return refWrapper{arg};
+}
+
+template<class T>
+void test(T arg)
+{
+    arg++;
+}
+int main(){
+
+    int x = 6;
+    test(myRef(x));
+    std::cout << x;//prints 7
+
+
+    auto rw1 = refWrapper(x);
+    auto rw2 = rw1;
+    test(rw2);
+    std::cout << "\n" << x;//prints 8
 }
